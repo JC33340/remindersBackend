1d09d901dd56194ce09ce77a9c45968d34ddc791
Jason Chan <jasonchanhw0531@gmail.com>
2024-05-28 16:34:08 +0100

    finished create user and authenticating functions

--- reminders_backend/backend/views.py
+++ reminders_backend/backend/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .serializers import ProfileSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+import json
 
 
 # Create your views here.
@@ -23,3 +27,18 @@ def get_profile(request):
     profile = user.profile
     serializer = ProfileSerializer(profile, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def create_user(request):
+    data = json.loads(request.body)
+    for key,value in data.items():
+        if value == "":
+            return Response('Please fill all fields', status=status.HTTP_400_BAD_REQUEST)
+    if data['password'] != data['passwordConfirmation']:
+        return Response('Password do not match',status=status.HTTP_400_BAD_REQUEST)
+    try:
+        user = User.objects.get(username=data['username'])
+        return Response('Username in use',status=status.HTTP_400_BAD_REQUEST)
+    except User.DoesNotExist:
+        user = User.objects.create_user(username=data['username'],password=data['password'])
+        return Response("User created",status=status.HTTP_200_OK)
