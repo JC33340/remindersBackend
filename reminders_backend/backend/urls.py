d8678dd0b15a993b56c1c6a6a4d944408cee539b
Jason Chan <jasonchanhw0531@gmail.com>
2024-06-11 13:58:58 +0100

    started creating add profile functions

--- reminders_backend/backend/urls.py
+++ reminders_backend/backend/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView
 urlpatterns = [
     path("",views.get_routes,name='get-routes'),
     path('token/',TokenObtainPairView.as_view(),name = 'token_obtain_pair'),
-    path('token/refresh',TokenRefreshView.as_view(),name='token_refresh'),
+    path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
     path('profile/', views.get_profile),
     path('create_user/',views.create_user)
 ]
--- reminders_backend/backend/views.py
+++ reminders_backend/backend/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponse,JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import ProfileSerializer
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 import json
+from backend.models import *
 
 
 # Create your views here.
@@ -20,13 +21,22 @@ def get_routes(request):
     ]
     return Response(routes)
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
 def get_profile(request):
-    user = request.user
-    profile = user.profile
-    serializer = ProfileSerializer(profile, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        user = request.user
+        try:
+            profile = user.profile
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data)
+        except User.profile.RelatedObjectDoesNotExist:
+            return Response({"user does not have a profile"}, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        #profile = Profile(user=request.user , first_name="jason",last_name="yes",email="foo@gmail.com")
+        #profile.save()
+        return Response({"profile created"}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def create_user(request):
